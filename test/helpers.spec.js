b426522fa9e5e42c7b6c3db6d3d816878db060cd
Jon Lauridsen <mail@jonlauridsen.com>
2017-08-29 00:19:59 +0100

    Nest helpers tests to further clarify sections

--- test/helpers.spec.js
+++ test/helpers.spec.js
@@ -1,29 +1,31 @@
 const assert = require("assert")
 const helpers = require("./helpers")
 
-describe("test.helpers#zip", function () {
-  it("combines 1 array", function () {
-    const result = helpers.zip(["a"])
-    assert.equal(result.length, 1)
-    assert.deepEqual(result[0], ["a"])
-  })
+describe("test.helpers", function () {
+  describe("#zip", function () {
+    it("combines 1 array", function () {
+      const result = helpers.zip(["a"])
+      assert.equal(result.length, 1)
+      assert.deepEqual(result[0], ["a"])
+    })
 
-  it("combines 2 arrays", function () {
-    const result = helpers.zip(["a"], [1])
-    assert.equal(result.length, 1)
-    assert.deepEqual(result[0], ["a", 1])
-  })
+    it("combines 2 arrays", function () {
+      const result = helpers.zip(["a"], [1])
+      assert.equal(result.length, 1)
+      assert.deepEqual(result[0], ["a", 1])
+    })
 
-  it("combines 3 arrays", function () {
-    const result = helpers.zip(["a"], [1], ["x"])
-    assert.equal(result.length, 1)
-    assert.deepEqual(result[0], ["a", 1, "x"])
-  })
+    it("combines 3 arrays", function () {
+      const result = helpers.zip(["a"], [1], ["x"])
+      assert.equal(result.length, 1)
+      assert.deepEqual(result[0], ["a", 1, "x"])
+    })
 
-  it("combines multiple items", function () {
-    const result = helpers.zip(["a", "b"], [1, 2])
-    assert.equal(result.length, 2)
-    assert.deepEqual(result[0], ["a", 1])
-    assert.deepEqual(result[1], ["b", 2])
+    it("combines multiple items", function () {
+      const result = helpers.zip(["a", "b"], [1, 2])
+      assert.equal(result.length, 2)
+      assert.deepEqual(result[0], ["a", 1])
+      assert.deepEqual(result[1], ["b", 2])
+    })
   })
 })

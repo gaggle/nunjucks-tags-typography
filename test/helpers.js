c6d93e39dbd80cffc4d1cbff8edcc7022cf0058e
Jon Lauridsen <mail@jonlauridsen.com>
2017-08-28 19:00:37 +0100

    Allow testcases to specify configuration via data-config- attributes

--- test/helpers.js
+++ test/helpers.js
@@ -1,16 +1,53 @@
 "use strict";
+const compare = require("dom-compare").compare
 const fs = require("fs")
+const path = require("path")
+const recursiveReaddirSync = require("recursive-readdir-sync")
+const reporter = require("dom-compare").GroupingReporter
+const xmldom = require("xmldom")
 
+const parser = new xmldom.DOMParser()
 
-/**
- * @returns {Promise}
- */
-exports.readFile = function (path, options) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, options, (err, data) => {
-      return err ? reject(err) : resolve(data)
-    })
+exports.extractConfig = function (xml) {
+  let conf = {}
+  if (!xml.attributes) return conf
+
+  let attributes = exports.nodeListToArr(xml.attributes)
+  if (!attributes.length) return conf
+
+  let searchString = "data-config-"
+  let filteredAttributes = attributes.filter(attr => attr.name.startsWith(searchString))
+
+  filteredAttributes.forEach(attr => {
+    let key = attr.name.replace(searchString, "")
+    conf[key] = attr.value === "true"
+    xml.removeAttribute(attr.name)
   })
+  return conf
+}
+
+exports.generateTestcaseName = function (xml) {
+  try {
+    let id = xml.getAttribute('id')
+    return `converts ${id.toLowerCase()}`
+  } catch (ex) {
+  }
+  return `converts`
+}
+
+exports.initCustomAsserts = function (assert) {
+  assert.xmlEqual = function (expected, actual) {
+    let comp = compare(expected, actual, {stripSpaces: true})
+    let message = `Documents are not equal
+${reporter.report(comp)}
+
+Expected document:
+${expected}
+
+Actual document:
+${actual}`
+    assert(comp.getResult(), message)
+  }
 }
 
 /**
@@ -25,9 +62,56 @@ exports.nodeListToArr = function (obj) {
 }
 
 /**
+ * @returns {Promise}
+ */
+exports.readFile = function (path, options) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, options, (err, data) => {
+      return err ? reject(err) : resolve(data)
+    })
+  })
+}
+
+exports.strToXML = function (str) {
+  return parser.parseFromString(str, "text/xml")
+}
+
+exports.walkElementPairs = function* (...filePaths) {
+  const content = filePaths.map(fp => fs.readFileSync(fp).toString())
+  const XMLs = content.map(d => [exports.strToXML(d)])
+  for (let item of exports.zip(...XMLs)) {
+    let childNodes = item.map(e => exports.nodeListToArr(e.childNodes))
+    for (let children of exports.zip(...childNodes)) {
+      yield children
+    }
+  }
+}
+
+exports.walkFixtures = function* (dir, opts = {}) {
+  opts.prioritise = opts.prioritise || opts.prioritize || []
+  dir = path.resolve(__dirname, dir)
+
+  const data = {}
+
+  for (let filepath of recursiveReaddirSync(dir)) {
+    let parentDirname = path.basename(path.dirname(filepath))
+    if (!data[parentDirname]) data[parentDirname] = []
+
+    let filename = path.basename(filepath)
+    opts.prioritise.indexOf(filename) !== 0 ?
+      data[parentDirname].push(filepath) :
+      data[parentDirname].unshift(filepath)
+  }
+
+  for (let name of Object.keys(data)) {
+    let [expected, src] = data[name]
+    yield {name, expected, src}
+  }
+}
+
+/**
  * @returns {Array}
  */
 exports.zip = function (...rows) {
   return rows[0].map((_, i) => rows.map(row => row[i]))
 }
-
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,68 +1,30 @@
 "use strict";
 const assert = require("assert")
-const compare = require('dom-compare').compare
 const Nunjucks = require("nunjucks-tags")
-const path = require("path")
-const recursiveReaddirSync = require('recursive-readdir-sync')
-const reporter = require('dom-compare').GroupingReporter
-const xmldom = require("xmldom")
 
 const helpers = require("./helpers")
 const tags = require("..")
 
-const parser = new xmldom.DOMParser()
-
-function* yieldFixtures(p) {
-  p = path.resolve(__dirname, p)
-
-  const data = {}
-
-  for (let fn of recursiveReaddirSync(p)) {
-    let shortDirname = path.basename(path.dirname(fn))
-    if (!data[shortDirname]) data[shortDirname] = []
-    fn.indexOf('expected.html') !== 0 ?
-      data[shortDirname].push(fn) :
-      data[shortDirname].unshift(fn)
-  }
-
-  for (let name of Object.keys(data)) {
-    let files = data[name]
-    yield {name, expected: files[0], src: files[1]}
-  }
-}
+helpers.initCustomAsserts(assert)
 
 describe("nunjucks-tags-typography", function () {
   let nunjucks
+  beforeEach(() => nunjucks = new Nunjucks())
 
-  beforeEach(() => {
-    nunjucks = new Nunjucks();
-    tags(nunjucks)
-  })
+  for (let fixture of helpers.walkFixtures("./fixtures", {prioritise: ["expected.html"]})) {
+    describe(fixture.name, function () {
+      for (let [expected, src] of helpers.walkElementPairs(fixture.expected, fixture.src)) {
+        if (!src || src.toString() === "\n") continue
 
-  for (let {name, expected, src} of yieldFixtures("./fixtures")) {
-    describe(name, function () {
-      it("converts to expected output", function () {
-        return Promise.all([helpers.readFile(expected), helpers.readFile(src)])
-          .then(buffers => {
-            let [expected, src] = buffers
-            return Promise.all([expected.toString(), nunjucks.render(src.toString())])
-          })
-          .then(strings => strings.map(s => parser.parseFromString(s, "test/xml")))
-          .then(DOMs => DOMs.map(d => helpers.nodeListToArr(d.childNodes)))
-          .then(children => {
-            helpers.zip(...children).forEach(item => {
-              const [expected, actual] = item
-              let comp = compare(expected, actual, {stripSpaces: true})
-              let message = `${reporter.report(comp)}
-Expected:
-${expected}
-
-Got:
-${actual}`
-              assert(comp.getResult(), message)
-            })
-          })
-      })
+        it(helpers.generateTestcaseName(src), function () {
+          let conf = helpers.extractConfig(src)
+          tags(nunjucks, conf)
+          return nunjucks.render(src.toString())
+            .then(helpers.strToXML)
+            .then(e => e.childNodes[0]) // We need the element itself, not top-level document
+            .then(actual => assert.xmlEqual(expected, actual))
+        })
+      }
     })
   }
 })
